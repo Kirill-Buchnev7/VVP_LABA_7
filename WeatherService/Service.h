70ead45bec31eb2da73bbad9a6fed193ec2fdfd6
”Kirill <kirillbuchnev18@gmail.com>
2025-05-08 18:42:55 +0300

    добавил класс JsonService и реализовал метод

--- WeatherService/Service.h
+++ WeatherService/Service.h
@@ -1,8 +1,39 @@
 #pragma once
+#include "Weather.h"
+#include <fstream>
+#include "json.hpp"
+
+using nlohmann::json;
 class Service
 {
 public:
-	virtual Weather getWeather(std::string s) = 0;
+	virtual Weather getWeather(string s) = 0;
 	virtual ~Service() {}
 };
+class JsonService :public Service
+{
+public:
+	virtual Weather getWeather(string s) override;
+	virtual ~JsonService() {};
+};
+
+
+Weather JsonService::getWeather(std::string s)
+{
+    std::ifstream fin(s);
+    if (!fin)
+        throw exception("error");
+
+    json j;
+    j = json::parse(fin);
+    std::string city = j["name"]; //  Киров
+    double lon = j["coord"]["lon"]; // 49.6601
+    double lat = j["coord"]["lat"]; // 58.5966  
+    double temperature = j["main"]["temp"]; // 5.69
+    std::string weather = j["weather"][0]["description"]; // дождь
+    double windSpeed = j["wind"]["speed"]; // 4.27
+    int clouds = j["clouds"]["all"]; // 100
+    return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
+}
+
 
